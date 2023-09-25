b95252710e2dcf9752e3ad0ed898932c45de2d28
Jonatan Storlund <storlundjonatan@gmail.com>
2023-09-25 15:21:50 +0300

    add RepositoryList

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,25 @@
+import Text from "./Text"
+
+const RepositoryItem = ({
+  fullName,
+  description,
+  language,
+  stargazersCount,
+  forksCount,
+  reviewCount,
+  ratingAverage
+}) => {
+  return (
+    <>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </>
+  )
+}
+
+export default RepositoryItem
